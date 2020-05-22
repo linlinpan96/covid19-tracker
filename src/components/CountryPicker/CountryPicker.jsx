880539374f1ed008b822ac7c2e5fa8185e5a10b8
Lin Lin Pan <linlinpan@GUILDENSTERN.local>
2020-05-22 10:08:38 -0400

    Built countrypicker, charts, cards.

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import { countries } from '../../api';
+
+const CountryPicker = () => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            setFetchedCountries(await countries);
+        }
+
+        fetchCountries();
+    }, [setFetchedCountries]);
+
+    return (
+        <div>
+            <FormControl className={styles.formControl}>
+                <NativeSelect>
+                    <option value="global">Global</option>
+                </NativeSelect>
+            </FormControl>
+        </div>
+    )
+}
+
+export default CountryPicker;
